b5236fa0194ad3ea00a3fe9361b6d5f9870d2e52
JoseBaena97 <josembaena97@gmail.com>
2026-03-02 20:02:52 +0000

    feat: implement randomcard generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    const suits = ["heart", "diamond", "spade", "club"];
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    
+    const icons = {
+        heart: "♥",
+        diamond: "♦",
+        spade: "♠",
+        club: "♣"
+    };
+
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+
+    const card = document.getElementById("card");
+    const topSuit = document.getElementById("top-suit");
+    const number = document.getElementById("number");
+    const bottomSuit = document.getElementById("bottom-suit");
+
+    card.classList.add(randomSuit);
+
+    topSuit.innerHTML = icons[randomSuit];
+    bottomSuit.innerHTML = icons[randomSuit];
+    number.innerHTML = randomValue;
 };
+
+
